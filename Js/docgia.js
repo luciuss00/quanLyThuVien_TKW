db53030bc598b6e1c6d788fd098fff93f0c19873
Nguyen Cuong <manhcuongnguyen0102@gmail.com>
2025-10-11 16:13:27 +0700

    doc gia add

---
+++ Js/docgia.js
@@ -0,0 +1,20 @@
+
+const userLocal = JSON.parse(localStorage.getItem("users")) || [];
+
+const tbody = document.getElementById("userTableBody");
+
+tbody.innerHTML = "";
+
+
+userLocal.forEach((user,index) => {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${user.userName}</td>
+    <td>${user.email}</td>
+    <td>${user.gender}</td>
+    <td>${user.tel}</td>
+    <td>${user.CCCD}</td>
+  `;
+  tbody.appendChild(row);
+});
